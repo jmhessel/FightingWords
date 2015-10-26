3e65781220b6eb8dcb8cd7c96a1eeb36a911fdbe
Jack Hessel <jmhessel@gmail.com>
2015-10-26 18:53:21 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# FIghtingWords
-Quick implementation of Monroe et al.'s algorithm for comparing languages
+# Fightin' Words
+Quick implementation of Monroe et al.'s algorithm for comparing languages.
+
+See their paper for more information. The citation is as follows:
+
+```
+@article{monroe2008fightin,
+  title={Fightin'words: Lexical feature selection and evaluation for identifying the content of political conflict},
+  author={Monroe, Burt L and Colaresi, Michael P and Quinn, Kevin M},
+  journal={Political Analysis},
+  volume={16},
+  number={4},
+  pages={372--403},
+  year={2008},
+  publisher={SPM-PMSAPSA}
+}
+```
+
+## The problem and its solution
+
+Say you have two groups of people talking, and you'd like to know
+which side is saying what. There are two problems that you might
+encounter when attempting this comparison:
+
+1. You have a disproportionate number of language samples from one side, so methods based on raw counts are not going to work.
+2. One side uses words that the other side doesn't, and it's not clear whether this occurs because the word is actually used more by one side, or because the word just happens to be rare.
+
+Monroe et al. solve the first problem by examining the usage *rates*
+of each word/n-gram, rather than the raw counts. They solve the second
+problem by introducing a smoothing [dirichlet](https://en.wikipedia.org/wiki/Dirichlet_distribution) prior on vocabulary items. These two solutions are unifed under a single, simple framework, which I have (hopefully) implemented here.
+
+## This code
+
+The function here takes in two lists of languages and outputs a list
+of tuples of the form `(n-gram, z-score)` where `n-gram` is an n-gram
+that you might be interested in and `z-score` is the model-based z-score that this n-gram is really being used by one side more than the other. Remember, any value outside of the range `[-1.96, 1.96]` is technically significant at the 95% level. However, be wary: this significance is a *model* based significance, so it only holds if your model holds. Using an *informative* prior as described in the paper might convince people that your model is more accurate, and your z-scores more valid :)
---
+++ fightingWords.py
@@ -0,0 +1,67 @@
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer as CV
+import string
+exclude = set(string.punctuation)
+
+def basicSanitize(inString):
+    '''Returns a very roughly sanitized version of the input string.'''
+    returnString = ' '.join(inString.encode('ascii', 'ignore').strip().split())
+    returnString = ''.join(ch for ch in returnString if ch not in exclude)
+    returnString = returnString.lower()
+    returnString = ' '.join(returnString.split())
+    return returnString
+
+def bayesCompareLanguage(l1, l2, ngram = 1, prior=.01, cv = None):
+    '''
+    Arguments:
+    - l1, l2; a list of strings from each language sample
+    - ngram; an int describing up to what n gram you want to consider (1 is unigrams,
+    2 is bigrams + unigrams, etc). Ignored if a custom CountVectorizer is passed.
+    - prior; either a float describing a uniform prior, or a vector describing a prior
+    over vocabulary items. If you're using a predefined vocabulary, make sure to specify that
+    when you make your CountVectorizer object.
+    - cv; a sklearn.feature_extraction.text.CountVectorizer object, if desired.
+
+    Returns:
+    - A list of length |Vocab| where each entry is a (n-gram, zscore) tuple.'''
+    if cv is None and type(prior) is not float:
+        print "If using a non-uniform prior:"
+        print "Please also pass a count vectorizer with the vocabulary parameter set."
+        quit()
+    l1 = [basicSanitize(l) for l in l1]
+    l2 = [basicSanitize(l) for l in l2]
+    if cv is None:
+        cv = CV(decode_error = 'ignore', min_df = 10, max_df = .5, ngram_range=(1,ngram),
+                binary = False,
+                max_features = 15000)
+    countsMat = cv.fit_transform(l1+l2).toarray()
+    # Now sum over languages...
+    vocabSize = len(cv.vocabulary_)
+    print "Vocab size is {}".format(vocabSize)
+    if type(priors) is float:
+        priors = np.array([prior for i in range(vocabSize)])
+    else:
+        priors = prior
+    zScores = np.empty(priors.shape[0])
+    countMatrix = np.empty([2, vocabSize], dtype=np.float32)
+    countMatrix[0, :] = np.sum(countsMat[:len(l1), :], axis = 0)
+    countMatrix[1, :] = np.sum(countsMat[len(l1):, :], axis = 0)
+    a0 = np.sum(priors)
+    n1 = 1.*np.sum(countMatrix[0,:])
+    n2 = 1.*np.sum(countMatrix[1,:])
+    print "Comparing language..."
+    for i in range(vocabSize):
+        #compute delta
+        term1 = np.log((countMatrix[0,i] + priors[i])/(n1 + a0 - countMatrix[0,i] - priors[i]))
+        term2 = np.log((countMatrix[1,i] + priors[i])/(n2 + a0 - countMatrix[1,i] - priors[i]))        
+        delta = term1 - term2
+        #compute variance on delta
+        var = 1./(countMatrix[0,i] + priors[i]) + 1./(countMatrix[1,i] + priors[i])
+        #store final score
+        zScores[i] = delta/np.sqrt(var)
+    indexToTerm = {v:k for k,v in cv.vocabulary_.iteritems()}
+    sortedIndices = np.argsort(zScores)
+    returnList = []
+    for i in sortedIndices:
+        returnList.append((indexToTerm[i], zScores[i]))
+    return returnList
