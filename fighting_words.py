776fe894e7b82704783af57b52a33e710715f124
Jack Hessel <jmhessel@gmail.com>
2016-04-26 01:57:00 -0400

    pythonized all of the naming

--- fighting_words.py
+++ fighting_words.py
@@ -3,15 +3,15 @@ from sklearn.feature_extraction.text import CountVectorizer as CV
 import string
 exclude = set(string.punctuation)
 
-def basicSanitize(inString):
+def basic_sanitize(in_string):
     '''Returns a very roughly sanitized version of the input string.'''
-    returnString = ' '.join(inString.encode('ascii', 'ignore').strip().split())
-    returnString = ''.join(ch for ch in returnString if ch not in exclude)
-    returnString = returnString.lower()
-    returnString = ' '.join(returnString.split())
-    return returnString
+    return_string = ' '.join(in_string.encode('ascii', 'ignore').strip().split())
+    return_string = ''.join(ch for ch in return_string if ch not in exclude)
+    return_string = returnString.lower()
+    return_string = ' '.join(return_string.split())
+    return return_string
 
-def bayesCompareLanguage(l1, l2, ngram = 1, prior=.01, cv = None):
+def bayes_compare_language(l1, l2, ngram = 1, prior=.01, cv = None):
     '''
     Arguments:
     - l1, l2; a list of strings from each language sample
@@ -28,40 +28,40 @@ def bayesCompareLanguage(l1, l2, ngram = 1, prior=.01, cv = None):
         print "If using a non-uniform prior:"
         print "Please also pass a count vectorizer with the vocabulary parameter set."
         quit()
-    l1 = [basicSanitize(l) for l in l1]
-    l2 = [basicSanitize(l) for l in l2]
+    l1 = [basic_sanitize(l) for l in l1]
+    l2 = [basic_sanitize(l) for l in l2]
     if cv is None:
         cv = CV(decode_error = 'ignore', min_df = 10, max_df = .5, ngram_range=(1,ngram),
                 binary = False,
                 max_features = 15000)
-    countsMat = cv.fit_transform(l1+l2).toarray()
+    counts_mat = cv.fit_transform(l1+l2).toarray()
     # Now sum over languages...
-    vocabSize = len(cv.vocabulary_)
-    print "Vocab size is {}".format(vocabSize)
+    vocab_size = len(cv.vocabulary_)
+    print "Vocab size is {}".format(vocab_size)
     if type(prior) is float:
-        priors = np.array([prior for i in range(vocabSize)])
+        priors = np.array([prior for i in range(vocab_size)])
     else:
         priors = prior
-    zScores = np.empty(priors.shape[0])
-    countMatrix = np.empty([2, vocabSize], dtype=np.float32)
-    countMatrix[0, :] = np.sum(countsMat[:len(l1), :], axis = 0)
-    countMatrix[1, :] = np.sum(countsMat[len(l1):, :], axis = 0)
+    z_scores = np.empty(priors.shape[0])
+    count_matrix = np.empty([2, vocab_size], dtype=np.float32)
+    count_matrix[0, :] = np.sum(counts_mat[:len(l1), :], axis = 0)
+    count_matrix[1, :] = np.sum(counts_mat[len(l1):, :], axis = 0)
     a0 = np.sum(priors)
-    n1 = 1.*np.sum(countMatrix[0,:])
-    n2 = 1.*np.sum(countMatrix[1,:])
+    n1 = 1.*np.sum(count_matrix[0,:])
+    n2 = 1.*np.sum(count_matrix[1,:])
     print "Comparing language..."
-    for i in range(vocabSize):
+    for i in range(vocab_size):
         #compute delta
-        term1 = np.log((countMatrix[0,i] + priors[i])/(n1 + a0 - countMatrix[0,i] - priors[i]))
-        term2 = np.log((countMatrix[1,i] + priors[i])/(n2 + a0 - countMatrix[1,i] - priors[i]))        
+        term1 = np.log((count_matrix[0,i] + priors[i])/(n1 + a0 - count_matrix[0,i] - priors[i]))
+        term2 = np.log((count_matrix[1,i] + priors[i])/(n2 + a0 - count_matrix[1,i] - priors[i]))        
         delta = term1 - term2
         #compute variance on delta
-        var = 1./(countMatrix[0,i] + priors[i]) + 1./(countMatrix[1,i] + priors[i])
+        var = 1./(count_matrix[0,i] + priors[i]) + 1./(count_matrix[1,i] + priors[i])
         #store final score
         zScores[i] = delta/np.sqrt(var)
-    indexToTerm = {v:k for k,v in cv.vocabulary_.iteritems()}
-    sortedIndices = np.argsort(zScores)
-    returnList = []
-    for i in sortedIndices:
-        returnList.append((indexToTerm[i], zScores[i]))
-    return returnList
+    index_to_term = {v:k for k,v in cv.vocabulary_.iteritems()}
+    sorted_indices = np.argsort(z_scores)
+    return_list = []
+    for i in sorted_indices:
+        return_list.append((index_to_term[i], z_scores[i]))
+    return return_list
